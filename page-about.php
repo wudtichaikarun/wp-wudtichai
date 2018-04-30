85f82f6381f3de16cfbb60b0b9a5b9a0d6f80896
Romantic <595159090004@dpu.ac.th>
2018-04-30 21:12:39 +0700

    add elementor to side

---
+++ page-about.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  Template Name: เกี่ยวกับเรา
+*/
+get_header(); ?>
+
+  <!-- SECTION MENU FEATURE -->
+  <main id="main">
+    <div class="row">
+      <!-- use plugin elementor add content -->
+      <?php the_content(); ?>
+    </div>
+  </main>
+
+<?php get_footer(); ?>
